ad315a78b0d89ffa36cc80b21eb4ead44a80f980
Atul Kumar <87774509+Atul-Kumar-Official@users.noreply.github.com>
2021-08-02 09:01:26 +0530

    Reduced the code a lot
    I am taking the input first and then we are calculating the results based on the operator...
    
    Also added an else statement

--- Calculator.py
+++ Calculator.py
@@ -1,27 +1,23 @@
-print("Welcome to the Calculator")
-print("1.Addition\n", "2.Subtraction\n", "3.Multiplication\n", "4.Division\n")
+print("Welcome to the Calculator!")
+print(" 1.Addition\n", "2.Subtraction\n", "3.Multiplication\n", "4.Division\n", "5.Exponent\n")
+
 a = int(input("Choose the operation: "))
 
+n1 = int(input("Enter the first no.: "))
+n2 = int(input("Enter the second no.: "))
+
 if a == 1:
-    n1 = int(input("Enter the first no.: "))
-    n2 = int(input("Enter the second no.: "))
     sum = n1 + n2
     print("Result: ", sum)
-
 elif a == 2:
-    n1 = int(input("Enter the first no.: "))
-    n2 = int(input("Enter the second no.: "))
-    sum = n1 - n2
-    print("Result: ", sum)
-
+    difference = n1 - n2
+    print("Result: ", difference)
 elif a == 3:
-    n1 = int(input("Enter the first no.: "))
-    n2 = int(input("Enter the second no.: "))
-    sum = n1 * n2
-    print("Result: ", sum)
-
+    product = n1 * n2
+    print("Result: ", product)
 elif a == 4:
-    n1 = int(input("Enter the first no.: "))
-    n2 = int(input("Enter the second no.: "))
-    sum = n1 // n2
-    print("Result: ", sum)
+    quotient = n1 / n2
+    remainder = n1 % n2
+    print("The quotient is: ", quotient, " and the remainder is: ", remainder)
+else:
+    print("Please enter a valid option")
