0638208b6c2d2c4defec2bfca3f10089a4429a77
Atul Kumar <87774509+Atul-Kumar-Official@users.noreply.github.com>
2021-08-02 09:08:47 +0530

    Reduced the code and added the exponent functionality
    I have reduced the code a lot and this program is faster than the previous one...
    I have also added a functionality to perform exponential functions and all...

--- Calculator.py
+++ Calculator.py
@@ -1,27 +1,26 @@
-print("Welcome to the Calculator")
-print("1.Addition\n", "2.Subtraction\n", "3.Multiplication\n", "4.Division\n")
+print("Welcome to the Calculator!")
+print(" 1.Addition\n", "2.Subtraction\n", "3.Multiplication\n", "4.Division\n", "5.Exponent\n")
+
 a = int(input("Choose the operation: "))
 
+n1 = int(input("Enter the first no.: "))
+n2 = int(input("Enter the second no.: "))
+
 if a == 1:
-    n1 = int(input("Enter the first no.: "))
-    n2 = int(input("Enter the second no.: "))
     sum = n1 + n2
     print("Result: ", sum)
-
 elif a == 2:
-    n1 = int(input("Enter the first no.: "))
-    n2 = int(input("Enter the second no.: "))
-    sum = n1 - n2
-    print("Result: ", sum)
-
+    difference = n1 - n2
+    print("Result: ", difference)
 elif a == 3:
-    n1 = int(input("Enter the first no.: "))
-    n2 = int(input("Enter the second no.: "))
-    sum = n1 * n2
-    print("Result: ", sum)
-
+    product = n1 * n2
+    print("Result: ", product)
 elif a == 4:
-    n1 = int(input("Enter the first no.: "))
-    n2 = int(input("Enter the second no.: "))
-    sum = n1 // n2
-    print("Result: ", sum)
+    quotient = n1 / n2
+    remainder = n1 % n2
+    print("The quotient is: ", quotient, " and the remainder is: ", remainder)
+elif a == 5:
+    result = n1 ** n2
+    print(n1, " to the power ", n2, " is ", result)
+else:
+    print("Please enter a valid option")
